a120202ccfba8fec7cbf7307d79ad3515ded2744
JPbast0z <joaopedro.silvabastos.splash@gmail.comyou@example.com>
2023-12-08 15:56:41 -0300

    relogio pronto

--- zapzap.py
+++ zapzap.py
@@ -2,7 +2,7 @@ import json
 import socket
 import threading
 import uuid
-import clock
+#import clock
 
 #IP e PORT
 HOST = '127.0.0.1'
@@ -10,10 +10,10 @@ PORT = int(input("Digite sua PORT"))
 SINC = 'SINCRONIZAR'
 mensagens_all = []
 #Contatos conhecidos (Dicionario com o endereço dos usuarios conectados) - host:porta
-membros_grupo = {1 : '127.0.0.1:1234', 2 : '127.0.0.1:5678', 3 : '127.0.0.1:9000'}
+membros_grupo = {1 : '127.0.0.1:1234', 2 : '127.0.0.1:5678', 3 : '127.0.0.1:9000', 4 : '127.0.0.1:1111'}
 
 #Histórico de mensagens
-historico_mensagens = [{'time' : 1, 'type' : 'msg', 'conteudo' : 'Olá', 'remetente' : ''}]
+historico_mensagens = [{'time' : 0, 'type' : 'msg', 'conteudo' : 'Olá', 'remetente' : '', 'id' : 'idTeste456156'}]
 
 #Server UDP
 recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,35 +34,77 @@ class LamportClock:
             self.value = max(self.value, received_time) + 1
             return self.value
 
-
+def sincronizar_relogio(clock):
+    sinc_mensagem = {'type': 'clockSync', 'clock': clock.value, 'host' : HOST, 'port' : PORT}
+    enviar_socket(sinc_mensagem)
 
 #Função que recebe mens
 def receber_mensagens(clock):
     while True:
         try:
-            mensagem, endereco = recv_socket.recvfrom(1024)
-            triagem_mensagens(mensagem)
+            print('1')
+            data, endereco = recv_socket.recvfrom(1024)
+            print(2)
+            triagem_mensagens(data, clock)
             #print("Membro: ", endereco, "\n", "[...]", mensagem, "\n")
-            clock.update()
+            
         except Exception as e:
             print(f"Erro ao receber mensagem: {e}")
 
-def triagem_mensagens(mensagem):
-    mensagem = json.loads(mensagem.decode())
+def enviar_socket(data):
+    mensagem_encode = json.dumps(data)
+    for i in membros_grupo:
+        if membros_grupo[i] != (str(HOST) + ':' + str(PORT)):
+            endereco_destino = membros_grupo[i].split(':')
+            destino_ip = endereco_destino[0]
+            print(endereco_destino)
+            destino_porta = int(endereco_destino[1])
+            enviar_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            enviar_socket.sendto(mensagem_encode.encode(), (destino_ip, destino_porta))
+            enviar_socket.close()
+            print("PASSSOUUUU")
+
+def enviar_mensagem(clock):
+    
+        #nome_destino = input("Digite o nome do destinatário: ")
+        mensagem = input("Digite a mensagem: ")
+        clock.increment()
+        id = gerar_id()
+        dict_mensagem = {'time' : clock.value, 'type' : 'msg', 'conteudo' : mensagem, 'remetente' : str(HOST) + ':' + str(PORT), 'id' : id}
+            
+        enviar_socket(dict_mensagem)
+
+        dict_mensagem.pop('type')
+        historico_mensagens.append(dict_mensagem)
+        exibir_mensagens()
+
+def triagem_mensagens(dados, clock):
+    mensagem = json.loads(dados.decode())
     if mensagem['type'] == 'msg':
         mensagem.pop('type')
+        clock.update(mensagem['time'])
         historico_mensagens.append(mensagem)
         exibir_mensagens()
-    elif mensagem['type'] == 'sync':
-        pass
-def exibir_mensagens(clock):
+    elif mensagem['type'] == 'clockSync':
+        clock.update(mensagem['clock'])
+        sinc_mensagem = {'type': 'updateClock', 'clock': clock.value}
+        sinc_mensagem = json.dumps(sinc_mensagem)
+        enviar_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        enviar_socket.sendto(sinc_mensagem.encode(), (mensagem['host'], mensagem['port']))
+        enviar_socket.close()
+    elif mensagem['type'] == 'updateClock':
+        clock.update(mensagem['clock'])
+        print('02 - ',HOST, ' : ', PORT, ' -> ', clock.value)
+        
+
+def exibir_mensagens():
     print('''
     -=-=-=-=-=-=--=--=-=-
         CHAT
     -=-=-=-=-=-=--=--=-=-
 ''')
     for i in historico_mensagens:
-        print('TIME: ', clock, ':', i['conteudo'])
+        print('TIME: ', i['time'], ':', i['conteudo'])
 
 def gerar_id(): 
     return str(uuid.uuid4())
@@ -74,9 +116,10 @@ def gerar_id():
 clock = LamportClock()
 
 #Thread para receber as mensagens a todo momento
-thread_receber = threading.Thread(target=receber_mensagens, args=(clock))
+thread_receber = threading.Thread(target=receber_mensagens, args=(clock,))
 thread_receber.start()
-
+sincronizar_relogio(clock)
+print(HOST, ' : ', PORT, ' -> ', clock.value)
 #Looping principal
 while True:
     comando = input("Digite 'conectar' para adicionar um par conhecido ou 'enviar' para enviar mensagem: ")
@@ -87,29 +130,6 @@ while True:
         cofre = len(membros_grupo) + 1
         membros_grupo[cofre] = str(HOST) + ':' + endereco_par
         print(f"Par {cofre} adicionado aos pares conhecidos.")
-    
-    elif comando == 'enviar':
-        #nome_destino = input("Digite o nome do destinatário: ")
-        mensagem = input("Digite a mensagem: ")
-        dict_mensagem = {'time' : 1, 'type' : 'msg', 'conteudo' : mensagem, 'remetente' : str(HOST) + ':' + str(PORT)}
-        
-        if len(membros_grupo) != 0:
-            lamport_time = clock.increment()
-            for i in membros_grupo:
-                if i != str(HOST) + ':' + str(PORT):
-                    endereco_destino = membros_grupo[i].split(':')
-                    mensagem_encode = json.dumps(dict_mensagem)
-                    destino_ip = endereco_destino[0]
-                    print(endereco_destino)
-                    destino_porta = int(endereco_destino[1])
-                    enviar_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    enviar_socket.sendto(mensagem_encode.encode(), (destino_ip, destino_porta))
-                    enviar_socket.close()
-                    print("PASSSOUUUU")
 
-            dict_mensagem.pop('type')
-            historico_mensagens.append(dict_mensagem)
-            exibir_mensagens(clock)
-        else:
-            print("iiiiiih")
-            #print(f"O par {nome_destino} não está na lista de pares conhecidos.")
+    elif comando == 'enviar':
+        enviar_mensagem(clock)
